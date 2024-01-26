fbb258e1a3a2916642d5e23345434aa473524782
manuprasad5016 <63902488+manuprasad5016@users.noreply.github.com>
2024-01-26 19:11:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -59,3 +59,24 @@ console.log('Manu 01');
   }
   
 });
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+// print stacktrace on error
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: err,
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Demo App Client listening on port ${port}!`)
+);
