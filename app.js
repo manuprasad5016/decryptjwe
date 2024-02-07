14e605a4b58ba7a8159cea389015366b4c3293b5
manuprasad5016 <63902488+manuprasad5016@users.noreply.github.com>
2024-02-07 11:32:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -36,41 +36,39 @@ app.use(cookieParser());
 app.post("/decryptCookie", async function (req, res, next) {
   try { 
 	const encrypted = req.body.jweEncrypted;
-	console.log('Encrypted OPRP Token-->'+encrypted);
-  
+	console.log('Encrypted OPRP Token-->'+encrypted); 
  
 	/* ***** FUNCTION TO RETURN STRINGIFY OUTPUT ***** */
 	function stringify(json) {
 	  return JSON.stringify(json,null);
-	}
- 
+	} 
   
 	/* ***** DECRYPT JWE ***** The JWE is decrypted here. */  
 	const decryptFunction = async function(jwe,privateKey){ 
-			const { plaintext, protectedHeader } = await jose.compactDecrypt(jwe, privateKey); 
-			console.log('plaintext-->'+plaintext);
-			const decryptedJwt = await new TextDecoder().decode(plaintext);
-			console.log('decryptedJwt Hmm-->'+decryptedJwt);
-			const jwtDecoded = await jwt.decode(decryptedJwt,{complete:true});
-			console.log('jwtDecoded-->'+jwtDecoded);
-			const nestedJwt = stringify(jwtDecoded);
-			console.log('nestedJwt-->'+nestedJwt);   
-			return nestedJwt;
+		const { plaintext, protectedHeader } = await jose.compactDecrypt(jwe, privateKey); 
+		console.log('plaintext-->'+plaintext);
+		const decryptedJwt = await new TextDecoder().decode(plaintext);
+		console.log('decryptedJwt Hmm-->'+decryptedJwt);
+		const jwtDecoded = await jwt.decode(decryptedJwt,{complete:true});
+		console.log('jwtDecoded-->'+jwtDecoded);
+		const nestedJwt = stringify(jwtDecoded);
+		console.log('nestedJwt-->'+nestedJwt);   
+		return {nestedJwt};
 	}
     
     // Execution of the program
 	const api = async function(){ 
 	const pkcs88 = '-----BEGIN PRIVATE KEY-----\n\
-					MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgg3x1HUUL3QkLStXP\n\
-					SVmnD8Dl6xHbsh7y5XuPU92H2kGhRANCAATyTtkjqH8ds9DB3oeVZnHHZDkiTOb7\n\
-					/8DZ4OHx+eFmJq8RvuxAQk5nSsQuew9nYTWMobEJgfqeWkE2xxcNWYc6\n\
-					-----END PRIVATE KEY-----'; 
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgg3x1HUUL3QkLStXP\n\
+SVmnD8Dl6xHbsh7y5XuPU92H2kGhRANCAATyTtkjqH8ds9DB3oeVZnHHZDkiTOb7\n\
+/8DZ4OHx+eFmJq8RvuxAQk5nSsQuew9nYTWMobEJgfqeWkE2xxcNWYc6\n\
+-----END PRIVATE KEY-----'; 
 	
 	const josePrivateKey = await jose.importPKCS8(pkcs88,'ES256'); 
-	const nestedJwt = await decryptFunction(encrypted, josePrivateKey);
+	const {nestedJwt} = await decryptFunction(encrypted, josePrivateKey);
 	console.log('Manu 02-->'+JSON.stringify(nestedJwt)); // log the data for demonstration purpose only
   
-    res.status(200).send(nestedJwt); //return personData
+    res.status(200).send(nestedJwt); //return Decrypted
     
 	}
 	api(); 
