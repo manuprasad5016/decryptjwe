51f37f11f164df6df2dc72449fc141b844424f8e
manuprasad5016 <63902488+manuprasad5016@users.noreply.github.com>
2024-06-14 22:32:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -66,24 +66,53 @@ app.post("/decryptCookie", async function (req, res, next) {
 	  console.log('Requested received in Node');
 	  const encrypted = req.body.jweEncrypted;
 	  console.log('Cookie Received from Req-->'+encrypted);
-
-var payload = new Dictionary<string, object>()
-{
-    { "sub", "mr.x@contoso.com" },
-    { "exp", 1300819380 }
-};
-	 
-    var publicKey = new Jwk(
-		crv : "P-256",
-		x : "98PMbEMr5XxFqmycz3SgRAYdnfgapPh8aBZeQFhT930",
-		y : "qHTt0ugI5tE0yx0gThIXfV-si4fa6lU9jj8us1xz7wY"
-	);
-
-string token = Jose.JWT.Encode(payload, publicKey, JweAlgorithm.ECDH_ES_A256KW, JweEncryption.A256CBC_HS512);
+	
+	  console.log('Request Headers-->'+JSON.stringify(req.headers)); 
+	  const cookieHeader = req.headers?.cookie;
+	  console.log('Cookie-->'+cookieHeader);
+	  console.log('Cookie full-->'+req.headers.cookie);
+  
+/*
+    ***** FUNCTION TO RETURN STRINGIFY OUTPUT *****
+*/
+function stringify(json) {
+  return JSON.stringify(json,null);
+} 
  
 
-	res.status(200).send(token);
-   
+/*
+    ***** DECRYPT JWE *****
+    The JWE is decrypted here.
+*/
+	  var actualPayload;
+const decryptFunction = async function(jwe,privateKey){ 
+  	const { plaintext, protectedHeader } = await jose.compactDecrypt(jwe, privateKey) 
+	//console.log('plaintext-->'+plaintext);
+  	const decryptedJwt = await new TextDecoder().decode(plaintext);
+	console.log('decryptedJwt-->'+decryptedJwt);
+  	const jwtDecoded = await jwt.decode(decryptedJwt,{complete:true});
+	//console.log('jwtDecoded-->'+jwtDecoded);
+  	const nestedJwt = stringify(jwtDecoded);
+	console.log('nestedJwt-->'+nestedJwt); 
+	actualPayload =''; 
+  	return {decryptedJwt, nestedJwt, actualPayload}
+}
+    
+    // Execution of the program
+const api = async function(){  
+	const pkcs88 = '-----BEGIN PRIVATE KEY-----\n\
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgg3x1HUUL3QkLStXP\n\
+SVmnD8Dl6xHbsh7y5XuPU92H2kGhRANCAATyTtkjqH8ds9DB3oeVZnHHZDkiTOb7\n\
+/8DZ4OHx+eFmJq8RvuxAQk5nSsQuew9nYTWMobEJgfqeWkE2xxcNWYc6\n\
+-----END PRIVATE KEY-----'; 
+	const josePrivateKey = await jose.importPKCS8(pkcs88,'ES256');
+ 
+  const {decryptedJwt, jwePayload, nestedJwt, actualPayload} = await decryptFunction(encrypted, josePrivateKey);
+  console.log('Decrypted JWT is-->'+JSON.stringify(nestedJwt)); 
+  
+    res.status(200).send(nestedJwt);
+	}
+	api(); 
   } 
   catch (error) {
     console.log("---MyInfo NodeJs Library Error---");
